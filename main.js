e86b841bcd1d80c58b82f8e66af2d595061a7355
maxjdev <maxjramosdev@gmail.com>
2023-09-25 21:00:01 -0300

    fix_exercise

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
-function validarFormulario() {
-    const nome = document.getElementById("nome").value;
-    const idade = parseInt(document.getElementById("idade").value);
-    const pessoas = parseInt(document.getElementById("pessoas").value);
+function validacaoJs() {
+    /* vars */
+    const age1 = document.getElementById("age1").value;
+    const age2 = document.getElementById("age2").value;
+    const name1 = document.getElementById("name1").value;
+    const name2 = document.getElementById("name2").value;
+    document.getElementById("compareButton").addEventListener("click", function(event) {
+        event.preventDefault();
+    })
 
-
-
-    const idadeMin = 18;
-    const limitePorQuarto = 3;
-
-    if (nome.split(" ").length < 2) {
-        document.querySelector(".error1").style.display = "block";
+    /* full name */
+    const name1split = name1.split(" ");
+    const name2split = name2.split(" ");
+    if (name1split.length <= 1 || name2split.length <= 1) {
+        if (name1split.length <= 1) {
+            document.querySelector(".error1").style.display = "block";
+        } else {
+            document.querySelector(".error2").style.display = "block";
+        }
         return false;
-    } else {
-        document.querySelector(".error1").style.display = "none";
+    }
+    document.querySelector(".error1").style.display = "none";
+    document.querySelector(".error2").style.display = "none";
+
+    /* age valid */
+    if (age1 > age2) {
+        alert("age1 > age2");
+    } else if (age2 > age1) {
+        alert("age2 > age1");
+    }else {
+        alert("equals");
     }
 
-    if (idade < idadeMin) {
-        document.querySelector(".error2").style.display = "block";
-        return false;
-    } else {
-        document.querySelector(".error2").style.display = "none";
-    }
-
-    if (pessoas > limitePorQuarto) {
-        document.querySelector(".error3").style.display = "block";
-        return false;
-    } else {
-        document.querySelector(".error3").style.display = "none";
-    }
-
-    alert("Reserva solicitada, aguarde nosso contato !")
-
+    /* end */
+    document.getElementById("myForm").reset();
     return true;
 }
