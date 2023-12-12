4b0c0121c532334aae6615f1c8ae97db423e9847
maxjdev <maxjramosdev@gmail.com>
2023-12-11 21:27:49 -0300

    add: exercise

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('btn-buscar-cep').addEventListener('click', function() {
-        const xhttp = new XMLHttpRequest();
-        const cep = document.getElementById('cep').value;
-        const endpoint= `https://viacep.com.br/ws/${}/json`;
+    const name = document.querySelector('#name');
+    const username = document.querySelector('#username');
+    const avatar = document.querySelector('#avatar');
+    const repos = document.querySelector('#repos');
+    const followers = document.querySelector('#followers');
+    const following = document.querySelector('#following');
+    const link = document.querySelector('#link');
 
-        xhttp.open('GET', endpoint);
-        xhttp.send();
-    })
+    fetch('https://api.github.com/users/maxjdev')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json){
+            name.innerText = json.name;
+            username.innerText = '@' + json.login;
+            avatar.src = json.avatar_url;
+            repos.innerHTML = json.public_repos;
+            followers.innerText = json.followers;
+            following.innerText = json.following;
+            link.href = json.html_url;
+        })
+        .catch(function(error) {
+            console.error("", error);
+            alert("Erro ao obter dados do GitHu. Por favor, tente novamente mais tarde.");
+        })
 })
